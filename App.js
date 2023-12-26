a5131413e4de678e04306aa74c0d850424eebbcf
Karl Loquias <loquiaskarl18@gmail.com>
2023-12-26 23:36:37 +0800

    update

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
+import data from './data';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const renderItem = ({ item }) => (
+    <View>
+      <TouchableOpacity onPress={() => toggleSelectedItem(item)}>
+        <Text style={styles.item}>{item.nickname}</Text>
+      </TouchableOpacity>
+
+      {selectedItem === item && (
+        <ScrollView style={styles.fullInfo}>
+          <Text style={styles.fullInfoText}>Full Information</Text>
+          <Text style={styles.infoDataTxt}>{`First Name: ${item.firstName}`}</Text>
+          <Text style={styles.infoDataTxt}>{`Last Name: ${item.lastName}`}</Text>
+          <Text style={styles.infoDataTxt}>{`Nickname: ${item.nickname}`}</Text>
+          <Text style={styles.infoDataTxt}>{`Course: ${item.course}`}</Text>
+          <Text style={styles.infoDataTxt}>{`Year: ${item.year}`}</Text>
+        </ScrollView>
+      )}
     </View>
   );
-}
+
+  const toggleSelectedItem = (item) => {
+    setSelectedItem((prevSelectedItem) => (
+      prevSelectedItem === item ? null : item
+    ));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Nickname List</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        style={styles.list}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: '#F0E5FF', // Light purple
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: '#FF69B4', // Hot pink
+    textAlign: 'center',
+  },
+  list: {
+    marginBottom: 16,
+  },
+  item: {
+    fontSize: 18,
+    marginBottom: 8,
+    backgroundColor: '#87CEFA', // Light sky blue
+    padding: 10,
+    borderRadius: 10,
+  },
+  fullInfo: {
+    backgroundColor: '#FFC0CB', // Pink
+    padding: 16,
+    borderRadius: 10,
+    marginBottom: 5,
+  },
+  fullInfoText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    color: '#4169E1', // Royal blue
+  },
+  infoDataTxt: {
+    fontSize: 17,
+    fontFamily: 'Trebuchet MS',
+    color: '#800080', // Purple
   },
 });
+
+export default App;
---
+++ data.js
@@ -0,0 +1,24 @@
+const data = 
+    [{"id":1,"firstName":"Sonia","lastName":"Cracknall","nickname":"Snickerdoodle","course":"English Literature","year":"4th"},
+{"id":2,"firstName":"Ardene","lastName":"Tudge","nickname":"Rascal","course":"Mathematics","year":"3rd"},
+{"id":3,"firstName":"Deonne","lastName":"Carr","nickname":"Bubbles","course":"Computer Science","year":"1st"},
+{"id":4,"firstName":"Sharleen","lastName":"Babinski","nickname":"Peaches","course":"Mathematics","year":"2nd"},
+{"id":5,"firstName":"Leonie","lastName":"Cliffe","nickname":"Daisy","course":"Physics","year":"3rd"},
+{"id":6,"firstName":"Dianne","lastName":"Mussettini","nickname":"Muffin","course":"Mathematics","year":"3rd"},
+{"id":7,"firstName":"Oneida","lastName":"Sumbler","nickname":"Muffin","course":"English Literature","year":"4th"},
+{"id":8,"firstName":"Clea","lastName":"Cashley","nickname":"Peaches","course":"Mathematics","year":"4th"},
+{"id":9,"firstName":"Brig","lastName":"Scandroot","nickname":"Muffin","course":"Computer Science","year":"3rd"},
+{"id":10,"firstName":"Bibi","lastName":"Tumbelty","nickname":"Muffin","course":"Physics","year":"3rd"},
+{"id":11,"firstName":"Jerry","lastName":"Corkell","nickname":"Daisy","course":"Mathematics","year":"2nd"},
+{"id":12,"firstName":"Loleta","lastName":"Iacofo","nickname":"Sunny","course":"English Literature","year":"4th"},
+{"id":13,"firstName":"Jany","lastName":"Metson","nickname":"Snickerdoodle","course":"Physics","year":"1st"},
+{"id":14,"firstName":"Ban","lastName":"Stevens","nickname":"Snickerdoodle","course":"Physics","year":"2nd"},
+{"id":15,"firstName":"Kale","lastName":"Marginson","nickname":"Snickerdoodle","course":"English Literature","year":"1st"},
+{"id":16,"firstName":"Clemens","lastName":"Yitzhak","nickname":"Noodle","course":"English Literature","year":"2nd"},
+{"id":17,"firstName":"Sharona","lastName":"Mockett","nickname":"Peaches","course":"Computer Science","year":"2nd"},
+{"id":18,"firstName":"Dylan","lastName":"Manifield","nickname":"Snickerdoodle","course":"Computer Science","year":"2nd"},
+{"id":19,"firstName":"Garnet","lastName":"Peele","nickname":"Sunny","course":"English Literature","year":"4th"},
+{"id":20,"firstName":"Claude","lastName":"Kiddie","nickname":"Rascal","course":"English Literature","year":"4th"}
+];
+  
+  export default data;
